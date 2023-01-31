014f19fb3a0f3efd651907767bcc5acc3af16f95
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-01-31 11:06:39 +0100

    added the code files for useRef explanation

---
+++ src/component/UseRef1.jsx
@@ -0,0 +1,26 @@
+import {useRef} from 'react'
+
+function UseRef1() {
+    const inputRef = useRef()
+    const paraRef = useRef()
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(inputRef.current.value)
+        // inputRef.current.value = 'Adeola'
+        paraRef.current.innerText = 'Thank you for submiting this form'
+    }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" ref={inputRef} id='name' className="form-control mb-2" />
+        <button type='submit' className="btn btn-primary">Submit</button>
+        <p ref={paraRef}>I am a paragraph</p>
+      </form>
+    </div>
+  )
+}
+
+export default UseRef1
---
+++ src/component/UseRef2.jsx
@@ -0,0 +1,22 @@
+import {useState, useEffect, useRef} from 'react'
+
+function UseRef2() {
+    const [name, setName] = useState('')
+    const render = useRef(1)
+    const prevName = useRef('')
+
+    useEffect(() => {
+        render.current = render.current + 1
+        prevName.current = name
+    }, [name])
+
+  return (
+    <div>
+      <h1>Render: {render.current}</h1>
+      <h3>Prev Name State: {prevName.current}</h3>
+      <input type="text" onChange={(e) => setName(e.target.value)} name={name} className="form-control mb-3" />
+    </div>
+  )
+}
+
+export default UseRef2
